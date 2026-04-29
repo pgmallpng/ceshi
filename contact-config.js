37afa8e5dbd88ae04ead5957715b708df357508d
pgmallpng <pgmallpng@gmail.com>
2026-04-29 12:01:55 +1000

    Update contact-config.js

--- contact-config.js
+++ contact-config.js
@@ -1,7 +1,7 @@
 var CONTACT_CONFIG = {
     hours: "10am – 10pm (PNG time)",
     contacts: [
-        { name: "Telegram",   url: "https://t.me/Mia0508",      icon: "📱" },
+        { name: "Telegram Mia",   url: "https://t.me/Mia0508",      icon: "📱" },
         { name: "Telegram",  url: "https://t.me/Mia_Claire_33",       icon: "📱" },
         { name: "Signal",            url: "https://signal.me/#eu/iawB5NudJJf08YQCU7heIQjavTxIyfiTVK1DT-8wZw-neEgqB89G7Iq7KwerJkSZ",     icon: "💬" },
         { name: "WhatsApp",         url: "https://wa.me/85257772131",   icon: "💬" }
